d3f863ea63cd8a032a0776e7aad92d7f1bc7b078
Ignacio Martinez <ignacio@protonmail.com>
2019-03-22 22:20:58 +0000

    updates example

--- example.R
+++ example.R
@@ -1,11 +1,37 @@
-IMS3::set.enviroment() # A personal package that sets my S3 credentials
+# IMS3::set.enviroment() # A personal package that sets my S3 credentials
+library(aws.ec2)
+library(aws.s3)
+IMSecrets::AWS()  # Personal package that setup the enviromental variables for aws
 
+# save to bucket
 put_object("./example.Rmd", bucket = "ignacios-test-bucket", object = "example.Rmd") # Saves the markdown to my S3 bucket
 
-remoter::batch(addr = IMSecrets::IMremoter()$addr, 
+# start AWS vm
+my_vm <- run_instances(image = "ami-05f39d58b5ff0d8e2", # My remoter ami
+                       type = "t2.medium",
+                       sgroup = "sg-073bb39404d5b2c8a") # A security group that only allows remoter connections
+
+vm_status <- instance_status(my_vm)
+
+while (is.null(vm_status$item$instanceStatus$status) | vm_status$item$instanceStatus$status !="ok") {
+  message(glue::glue("Instance is {vm_status$item$instanceStatus$status}."))
+  Sys.sleep(20)
+  vm_status <- instance_status(my_vm)
+  if(vm_status$item$instanceStatus$status =="ok") message("The instance is ready.")
+}
+
+# Run job 
+
+remoter::batch(addr = get_instance_public_ip(my_vm)[[1]], 
                port = IMSecrets::IMremoter()$port, 
                password = IMSecrets::IMremoter()$password, 
-               file = "script.R") # Runs my script on my remote R instance
+               file = "script.R") 
 
-aws.s3::s3sync(bucket = "ignacios-test-bucket", direction = "download") # Download the results
+# Download the results
+
+aws.s3::s3sync(bucket = "ignacios-test-bucket", direction = "download") 
+
+# Terminate the instance
+
+terminate_instances(my_vm)
 
